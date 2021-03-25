d33de6b30bd2657b75fbdfab0a5fb553bb578c6c
Roboticol <kaushikguha68@gmail.com>
2021-03-25 20:18:34 +0530

    added more item functionality

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,22 +6,37 @@ public class Item : MonoBehaviour
 {
 
     public bool placed;
+    public float offsetX;
+
+    public static int key = 0;
+    public int mainKey = 0;
 
     Vector2 prevPos;
 
     // Start is called before the first frame update
     void Start()
     {
+        key++;
+        mainKey = key;
+
+        transform.position = new Vector2(mainKey - offsetX, 0);
+        GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+
         prevPos = transform.position;
+
+        Debug.Log(key);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(mainKey.ToString()))
+        {
+            Place();
+        }
     }
 
-    void OnMouseDown()
+    void Place()
     {
         if(!placed)
         {
@@ -31,6 +46,7 @@ public class Item : MonoBehaviour
         {
             Inventory.DeleteInventoryObj(gameObject, prevPos);
         }
+        Debug.Log("pressed: " + Input.inputString);
         placed = !placed;
     }
 }
